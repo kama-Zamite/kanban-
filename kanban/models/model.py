d29e80740aebb0d690727556ec63efd96208bbf6
kama-Zamite <pedrozamite1@gmail.com>
2026-05-07 13:47:28 -0400

    resolucao de erros na migrasao de forma async

---
+++ kanban/models/model.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship
+)
+from ..db.base import Base
+from typing import List
+
+class User(Base):
+    __tablename__ = 'users'
+    id : Mapped[int] = mapped_column(init=False, primary_key=True)
+    name : Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    senha: Mapped[str] = mapped_column(nullable=False)
+    admin: Mapped[bool] = mapped_column(default=False)
+    active: Mapped[bool] = mapped_column(default=True)
+    avatar_url: Mapped[str | None] = mapped_column(nullable=False, default=None)
+
+    tasks_assigend: Mapped[List["Task"]] = relationship(back_populates="assingee", default_factory=List)
+
+
+class Task(Base):
+    ...
